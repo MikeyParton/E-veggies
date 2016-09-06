35665aad137d645961f0edb0180881b20b16017a
Mike Parton <mikeparton@live.com.au>
2016-09-05 23:49:34 -0300

    Updated assets

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "i0KU4jLYYjWzxpTraWviw", "djMu7QStnBK89MkfIg78tIZ4sFhmGOYjdCjDD0Wsc"
-  provider :facebook, "332742943732794", "a3e279d4b9320bc5b56e3fea68533c4f"
+  provider :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_secret_key
+  provider :twitter, Rails.application.secrets.twitter_app_id, Rails.application.secrets.twitter_sectet_key
 end
